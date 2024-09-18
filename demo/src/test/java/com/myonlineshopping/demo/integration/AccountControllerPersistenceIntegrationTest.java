d05048f0b2e194657d22c6b76960d3dce53eda0f
unknown <eduquiroga8@gmail.com>
2024-09-18 11:38:29 +0200

    Cambios

---
+++ demo/src/test/java/com/myonlineshopping/demo/integration/AccountControllerPersistenceIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.myonlineshopping.demo.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import static net.bytebuddy.matcher.ElementMatchers.is;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class AccountControllerPersistenceIntegrationTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+
+
+
+    @Test
+    void givenGetCuentasDeUnUsuario_whenInvalidOwnerId_then () throws Exception {
+
+        /*Product nuevoProd = new Product();
+        nuevoProd.setName("Nuevo Prod");
+        repository.save(nuevoProd);
+        mvc.perform(get("/products").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].name", is("Nuevo Prod")));*/
+
+        mvc.perform(get("/accounts/owner/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect((ResultMatcher) content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect((ResultMatcher) jsonPath("$[0].id", is("4")));
+    }
+
+
+}
